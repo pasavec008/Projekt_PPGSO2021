f7c5120ac6455f13be8d521a7fc6a9e3bf83b7a2
Samuel Schmidt <55737445+schmidt-8@users.noreply.github.com>
2021-11-16 17:04:56 +0100

    Add files via upload

---
+++ kostol/cmake-build-debug/CMakeFiles/kostol.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Samuel Schmidt/Desktop/Files/UNI/3.rocnik__zimny/PPGSO/kostol/main.cpp" "C:/Users/Samuel Schmidt/Desktop/Files/UNI/3.rocnik__zimny/PPGSO/kostol/cmake-build-debug/CMakeFiles/kostol.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ kostol/cmake-build-debug/CMakeFiles/kostol.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kostol.dir/main.cpp.obj"
+  "kostol.exe"
+  "kostol.exe.manifest"
+  "kostol.pdb"
+  "libkostol.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kostol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
