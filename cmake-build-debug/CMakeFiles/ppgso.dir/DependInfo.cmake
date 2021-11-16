00acfe5963985faec48bfef0fdc4add281275118
pasavec008 <m.adamovic999@gmail.com>
2021-11-16 18:14:12 +0100

    fix n.2

---
+++ cmake-build-debug/CMakeFiles/ppgso.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/ppgso/image.cpp" "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image.cpp.obj"
+  "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/ppgso/image_bmp.cpp" "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image_bmp.cpp.obj"
+  "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/ppgso/image_raw.cpp" "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image_raw.cpp.obj"
+  "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/ppgso/mesh.cpp" "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/mesh.cpp.obj"
+  "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/ppgso/shader.cpp" "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/shader.cpp.obj"
+  "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/ppgso/texture.cpp" "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/texture.cpp.obj"
+  "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/ppgso/tiny_obj_loader.cpp" "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/tiny_obj_loader.cpp.obj"
+  "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/ppgso/window.cpp" "D:/skola2021_2022/predmety/PPGSO/Projekt_PPGSO2021-cau/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/window.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "GLM_FORCE_RADIANS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ppgso"
+  "../dependencies/include"
+  "../"
+  "."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/projekt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt.dir/src/projekt/projekt.cpp.obj"
+  "libprojekt.dll.a"
+  "projekt.exe"
+  "projekt.exe.manifest"
+  "projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
